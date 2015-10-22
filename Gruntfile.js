94e38f8bcba358e95142c731b8e00d1ec475c0de
André Goulart <andre.goulart@b2wdigital.com>
2015-10-22 19:57:54 -0200

    atualizando master

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,43 +1,43 @@
 module.exports = function(grunt) {
 
-	require('load-grunt-tasks')(grunt);
+  require('load-grunt-tasks')(grunt);
 
-	grunt.initConfig({
-		'http-server': {
-			'dev': {
-				root: './',
-				port: 8080,
-				host: "0.0.0.0",
-				autoIndex: true,
-				ext: "html",
-				runInBackground: true,
-				openBrowser : true
-			}
-		},
-		watch:{
-			options: {
-				livereload: false
-			},
-			styles: {
-				files: ['**/*.css'],
-				tasks: []
-			},
-			scripts: {
-				files: ['**/*.js'],
-				tasks: [],
-				options: {
-					livereload: true
-				}
-			},
-			index: {
-				files: ['index.html'],
-				tasks: [],
-				options: {
-					livereload: true
-				}
-			},
-		}
-	});
+  grunt.initConfig({
+    'http-server': {
+      'dev': {
+        root: './',
+        port: 8080,
+        host: "0.0.0.0",
+        autoIndex: true,
+        ext: "html",
+        runInBackground: true,
+        openBrowser : true
+      }
+    },
+    watch:{
+      options: {
+        livereload: false
+      },
+      styles: {
+        files: ['**/*.css'],
+        tasks: []
+      },
+      scripts: {
+        files: ['**/*.js'],
+        tasks: [],
+        options: {
+          livereload: true
+        }
+      },
+      index: {
+        files: ['index.html'],
+        tasks: [],
+        options: {
+          livereload: true
+        }
+      },
+    }
+  });
 
-	grunt.registerTask('default', ['http-server', 'watch'])
+  grunt.registerTask('default', ['http-server', 'watch'])
 };
