0bd4f6801cf8be7eb5fe36ef095d463560108e96
André Filipe Goulart <afgoulart.rj@gmail.com>
2015-10-22 00:43:28 -0200

    Restruturando os arquivos e adicionando mais slides

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,113 +3,41 @@ module.exports = function(grunt) {
 	require('load-grunt-tasks')(grunt);
 
 	grunt.initConfig({
-		bump: {
+		'http-server': {
+			'dev': {
+				root: './',
+				port: 8080,
+				host: "0.0.0.0",
+				autoIndex: true,
+				ext: "html",
+				runInBackground: true,
+				openBrowser : true
+			}
+		},
+		watch:{
 			options: {
-				files: ['package.json', 'bower.json'],
-				commitFiles: ['package.json', 'bower.json'],
-				pushTo: 'origin'
-			}
-		},
-		copy: {
-			prepare: {
-				files: [{
-					src: [
-						'**',
-						'!node_modules/**',
-						'!bower_components/**',
-						'!Contributing.md',
-						'!Gruntfile.js',
-						'!License.md',
-						'!Readme.md',
-						'!bower.json',
-						'!package.json'
-					],
-					dest: 'temp/pres/'
-				},{
-					expand: true,
-					cwd: 'node_modules/shower-core/',
-					src: [
-						'**',
-						'!package.json',
-						'!Readme.md'
-					],
-					dest: 'temp/pres/shower/'
-				},{
-					expand: true,
-					cwd: 'node_modules/shower-ribbon/',
-					src: [
-						'**',
-						'!package.json',
-						'!Readme.md'
-					],
-					dest: 'temp/pres/shower/themes/ribbon/'
-				},{
-					expand: true,
-					cwd: 'node_modules/shower-bright/',
-					src: [
-						'**',
-						'!package.json',
-						'!Readme.md'
-					],
-					dest: 'temp/pres/shower/themes/bright/'
-				}]
-			}
-		},
-		replace: {
-			core: {
-				src: 'temp/pres/index.html',
-				overwrite: true,
-				replacements: [{
-					from: /(node_modules|bower_components)\/shower-core/g,
-					to: 'shower'
-				},{
-					from: /(node_modules|bower_components)\/shower-(ribbon|bright)/g,
-					to: 'shower/themes/$2'
-				}]
+				livereload: false
 			},
-			themes: {
-				src: 'temp/pres/shower/themes/*/index.html',
-				overwrite: true,
-				replacements: [{
-					from: '../shower-core', to: '../..'
-				}]
-			}
-		},
-		'gh-pages': {
-			options: {
-				base: 'temp/pres',
-				clone: 'temp/clone'
+			styles: {
+				files: ['**/*.css'],
+				tasks: []
 			},
-			src: ['**']
-		},
-		compress: {
-			shower: {
+			scripts: {
+				files: ['**/*.js'],
+				tasks: [],
 				options: {
-					archive: 'archive.zip'
-				},
-				files: [{
-					expand: true,
-					cwd: 'temp/pres/',
-					src: '**',
-					dest: '.'
-				}]
-			}
-		},
-		clean: ['temp']
+					livereload: true
+				}
+			},
+			index: {
+				files: ['index.html'],
+				tasks: [],
+				options: {
+					livereload: true
+				}
+			},
+		}
 	});
 
-	grunt.registerTask('publish', [
-		'copy',
-		'replace',
-		'gh-pages',
-		'clean'
-	]);
-
-	grunt.registerTask('archive', [
-		'copy',
-		'replace',
-		'compress',
-		'clean'
-	]);
-
+	grunt.registerTask('default', ['http-server', 'watch'])
 };
